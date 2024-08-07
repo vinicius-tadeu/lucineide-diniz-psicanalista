9d15de771e9b0eec44a55c3d99a6ae6df56f12cd
Vinicius Tadeu Soares Silva <001833@CTJ.ORG.BR>
2024-08-07 19:01:11 -0300

    Atualizações: Adição de menu hamburguer

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var menu = document.getElementById("menu-hamburguer");
-var aberto = false;
-console.log(menu);
+var menuAberto = false;
 
-function abrir() {
-  if (!aberto) {
+function abrirMenu() {
+  if (!menuAberto) {
     menu.style.height = "300px";
     menu.style.width = "300px";
     menu.style.padding = "20px";
-    aberto = true;
+    menuAberto = true;
   } else {
     menu.style.height = "20px";
     menu.style.width = "20px";
     menu.style.padding = "0px";
-    aberto = false;
+    menuAberto = false;
   }
 }
